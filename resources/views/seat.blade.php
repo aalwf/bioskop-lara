7f9970ee738547a2a722685a20f09aadd1373c38
Blurryface <2223594.elgin@smkn-2sbg.sch.id>
2024-05-03 09:25:02 +0700

    seat active button

--- resources/views/seat.blade.php
+++ resources/views/seat.blade.php
@@ -5,19 +5,19 @@
         <h1 class="text-2xl font-bold text-indigo-500">{{ $movie->name }} ({{ $movie->studio_name }})</h1>
         <p class="text-lg">{{ $date }} <span class="mx-5">|</span> {{ $time }}</p>
     </div>
-    <div class="grid grid-cols-2 gap-6 mt-5">
+    <div class="grid md:grid-cols-2 lg:grid-cols-2 sm:grid-rows-2 sm:grid-cols-1 mt-5 gap-4">
         <?php $i = "A" ?>
         @for($r = 0; $r < $movie->studio_capacity / 12; $r++)
-            <div class="flex flex-wrap items-center justify-center gap-6">
+            <div class="flex flex-nowrap items-center justify-evenly">
                 @for($c = 1; $c <= 6; $c++)
-                <button class="bg-indigo-500 hover:bg-indigo-700 text-white p-3 rounded">
+                <button class="seat" name="seat" value="{{ $i }}0{{ $c }}">
                     {{ $i }}0{{ $c }}
                 </button>
                 @endfor
             </div>
-            <div class="flex flex-wrap items-center justify-center gap-6">
+            <div class="flex flex-nowrap items-center justify-evenly">
                 @for($c = 7; $c <= 12; $c++)
-                    <button class="bg-indigo-500 hover:bg-indigo-700 text-white rounded p-3">
+                    <button class="seat" name="seat" value="{{ $i }}{{ ($c > 9 ? $c : '0' . $c) }}">
                         {{ $i }}{{ ($c > 9 ? $c : '0' . $c) }}
                     </button>
                 @endfor
@@ -25,4 +25,75 @@
             <?php $i++; ?>
         @endfor
     </div>
+    <div class="flex items-center justify-center mt-12">
+        <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" type="button" class="bg-indigo-500 text-gray-100 py-2 px-3 rounded shadow-md hover:bg-indigo-400">Selesai Memilih</button>
+    </div>
+    
+    <!-- Main modal -->
+    <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                        Create New Product
+                    </h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <form class="p-4 md:p-5">
+                    <div class="grid gap-4 mb-4 grid-cols-2">
+                        <div class="col-span-2">
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                        </div>
+                        <div class="col-span-2 sm:col-span-1">
+                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                            <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
+                        </div>
+                        <div class="col-span-2 sm:col-span-1">
+                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                            <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                <option selected="">Select category</option>
+                                <option value="TV">TV/Monitors</option>
+                                <option value="PC">PC</option>
+                                <option value="GA">Gaming/Console</option>
+                                <option value="PH">Phones</option>
+                            </select>
+                        </div>
+                        <div class="col-span-2">
+                            <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
+                            <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"></textarea>                    
+                        </div>
+                    </div>
+                    <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                        Add new product
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div> 
+  
+
+    <script>
+        // Get the button
+        const buttons = document.querySelectorAll(".seat");
+
+        // Loop melalui setiap tombol
+        for (let i = 0; i < buttons.length; i++) {
+            // Tambahkan event listener untuk setiap tombol
+            buttons[i].addEventListener("click", function() {
+                // Tambahkan kelas 'active' ke tombol yang diklik
+                console.log(this.value);
+                this.classList.toggle("seat-active");
+            });
+        }
+    </script>
 @endsection
